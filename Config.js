068aa095e37448af557952659160ebfd5ce52b64
Christopher Wang <wang967@usc.edu>
2018-11-25 18:00:39 -0800

    added config file

---
+++ Config.js
@@ -0,0 +1,38 @@
+export const Colors = {
+    primary: '#15566A',
+    secondary: '#FFC857',
+    tertiary: '#4B3F72',
+    tabBarDeselected: '#119DA4',
+    red: '#E63462'
+};
+
+export const Fonts = {
+    size: {
+        small: 16,
+        regular: 18,
+        large: 24,
+        subtitle: 28,
+        headingSmall: 20,
+        description: 26,
+        title: 48,
+    },
+    weight: {
+        light: '300',
+        regular: '500',
+        medium: '600',
+        bold: '800',
+    },
+    fontFamily: 'Helvetica',
+};
+
+export const ViewStyles = {
+    horizontalPadding: {
+        flex: 1,
+        backgroundColor: '#FFFFFF',
+        paddingHorizontal: 10,
+    },
+    noPadding: {
+        flex: 1,
+        backgroundColor: '#FFFFFF',
+    },
+};
